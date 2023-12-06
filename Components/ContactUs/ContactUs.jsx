05e7115593f428b7b6406e12c33dbda20812111f
Ashraful Islam <ashrafulislam21211@gmail.com>
2023-12-06 20:52:55 +0600

    revamp avoidrafa 2.0

--- Components/ContactUs/ContactUs.jsx
+++ Components/ContactUs/ContactUs.jsx
@@ -1,11 +1,14 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import Calendar from "react-calendar";
+import emailjs from "@emailjs/browser";
 // import "react-calendar/dist/Calendar.css";
 const ContactUs = () => {
   const [value, onChange] = useState(new Date());
   const [Information, setInformation] = useState({});
+  console.log(Information);
+  const form = useRef();
   const getInformation = (event) => {
     const { name, value } = event.target;
     setInformation((prev) => ({
@@ -15,10 +18,26 @@ const ContactUs = () => {
   };
   const formhandaler = (event) => {
     event.preventDefault();
-    event.target.Concertname.value = "";
-    event.target.concertdate.value = "";
-    event.target.contactnumber.value = "";
-    setInformation({});
+
+    emailjs
+      .sendForm(
+        "service_q76a1ha",
+        "template_d1dauak",
+        form.current,
+        "iY2KxbK_gADBnaSis"
+      )
+      .then(
+        (result) => {
+          alert("Mail Sent Successfully");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    // event.target.Concertname.value = "";
+    // event.target.concertdate.value = "";
+    // event.target.contactnumber.value = "";
+    // setInformation({});
   };
   return (
     <div className="py-20 px-[50px] " id="contact">
@@ -32,6 +51,7 @@ const ContactUs = () => {
       </div>
       <div className="flex  justify-center  gap-x-[16px] md:gap-x-40 lg:gap-x-56 my-10">
         <form
+          ref={form}
           onSubmit={formhandaler}
           className="flex flex-col  2xl:gap-y-10 w-[9rem] lg:w-80"
         >
@@ -39,9 +59,24 @@ const ContactUs = () => {
             htmlFor=""
             className="text-white py-2  text-[16px] sm:text-[16px]"
           >
+            Name/Organization Name
+            <input
+              type="text"
+              name="name"
+              required
+              placeholder="Type your Show/Concert location"
+              className="text-gray-300 text-[12px]   bg-transparent placeholder:bg-transparent placeholder:text-gray-300  placeholder:text-[12px] sm:placeholder:text-[12px] w-[7rem] lg:w-80   outline-none border-b  placeholder:uppercase  border-b-slate-500"
+              onChange={getInformation}
+            />
+          </label>
+          <label
+            htmlFor=""
+            className="text-white py-2  text-[16px] sm:text-[16px]"
+          >
             Show/Concert location
             <input
               type="text"
+              required
               name="Concertname"
               placeholder="Type your Show/Concert location"
               className="text-gray-300 text-[12px]   bg-transparent placeholder:bg-transparent placeholder:text-gray-300  placeholder:text-[12px] sm:placeholder:text-[12px] w-[7rem] lg:w-80   outline-none border-b  placeholder:uppercase  border-b-slate-500"
@@ -54,7 +89,8 @@ const ContactUs = () => {
           >
             Concert Date
             <input
-              type="text"
+              type="date"
+              required
               name="concertdate"
               placeholder="Concert Date"
               className="text-gray-300 text-[12px]  bg-transparent placeholder:bg-transparent placeholder:text-gray-300  placeholder:text-[12px] sm:placeholder:text-[12px]  w-[7rem] lg:w-80   outline-none border-b border-b-slate-500"
@@ -68,6 +104,7 @@ const ContactUs = () => {
             Contact Number
             <input
               type="text"
+              required
               name="contactnumber"
               placeholder="Contact Number"
               className="text-gray-300 text-[12px]  bg-transparent placeholder:bg-transparent placeholder:text-gray-300  placeholder:text-[12px] sm:placeholder:text-[12px]  w-[7rem] lg:w-80   outline-none border-b border-b-slate-500"
